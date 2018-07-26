4623e9b5c609e1d5f0cd8d1371d18b9229f54945
coloro <colorolabs@gmail.com>
2018-07-26 14:39:31 -0400

    nicknames

--- contracts/MPresto.sol
+++ contracts/MPresto.sol
@@ -14,11 +14,21 @@ contract MPresto {
 
   Item[] public items;
 
+  mapping(bytes32 => address) public nicknames;
+  mapping(address => bytes32) public ownerNickname;
+
   function createItem(string _name, uint _quantity) public {
     uint id = items.push(Item(_name, _quantity)) - 1;
     itemToOwner[id] = msg.sender;
     ownerItemCount[msg.sender]++;
     emit NewItem(id, _name, _quantity);
+  }
+
+  function getItem(uint _itemId) public view returns (string, uint, address, bytes32) {
+    Item storage item = items[_itemId];
+    address owner = itemToOwner[_itemId];
+    bytes32 nickname = ownerNickname[owner];
+    return (item.name, item.quantity, owner, nickname);
   }
 
   function getItemsByOwner(address _owner) public view returns (uint[]) {
@@ -42,6 +52,14 @@ contract MPresto {
     emit Transfer(msg.sender, _to, _itemId);
   }
 
+  function setNickname(bytes32 _nickname) public {
+    require(nicknames[_nickname] == 0x0);
+    nicknames[_nickname] = msg.sender;
+    ownerNickname[msg.sender] = _nickname;
+    emit ChangeNickname(_nickname, msg.sender);
+  }
+
   event NewItem(uint id, string name, uint quantity);
   event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+  event ChangeNickname(bytes32 _nickname, address indexed _owner);
 }
